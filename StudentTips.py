f66dfbec5adf4b68796962438ef07b5fca7669f5
giuspesch <gius.pesch@gmail.com>
2015-12-06 10:25:48 +0100

    cookie management

--- StudentTips.py
+++ StudentTips.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
 
 app = Flask(__name__)
 
@@ -9,10 +9,37 @@ templates, instead, contains all dynamic folders
 def name of the page():
     return render.template('pageName.html',...)"""
 
+def cookie_status(response_page, response_title):
+    # look if there are cookies stored
+    user_mail = request.cookies.get('user')
+    #check if the user has logged out
+    if('logout' in request.form):
+        response = make_response(render_template(response_page, title=response_title, username=''))
+        response.set_cookie('user', '', expires=0)
+        return response
+    #check if the user has logged in
+    if (not user_mail) and ('input_email' in request.form) and ('input_password' in request.form):
+        user_mail = request.form['input_email']
+        user_password = request.form['input_password']
+        # here we should include communication with DB, to check if credentials are correct
+        """user = User.query.filter_by((email=user_email) & (password=user_password)).first()"""
+        """update is_logged"""
+        # in case of correct login, we set the cookies
+        """response = make_response(render_template('index.html', title='Studentips', username=user.first_name))"""
+        response = make_response(render_template(response_page, title=response_title, username=user_mail))
+        if 'remember_me' in request.form: #wait for HTML for signed user to check if this is correct
+            response.set_cookie('user', user_mail)
+        return response
+    elif not user_mail:
+         return render_template(response_page, title=response_title, username='')
 
-@app.route('/')
+    return render_template(response_page, title=response_title, username=user_mail)
+
+
+@app.route('/', methods=["POST", "GET"])
 def homepage():
-    return render_template('index.html', title='Studentips')
+    #login and cookie management
+    return cookie_status("index.html", "Studentips")
 
 @app.route('/login')
 def login():
